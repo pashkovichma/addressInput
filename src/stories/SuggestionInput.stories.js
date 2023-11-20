e7f062b58064ced2626e889feea3b338eb621c5c
Maria <pashkovichma@gmail.com>
2023-11-20 15:00:34 +0100

    storybook added

---
+++ src/stories/SuggestionInput.stories.js
@@ -0,0 +1,8 @@
+import SuggestionInput from "../components/SuggestionInput";
+
+export default {
+  title: "SuggestionInput",
+  component: SuggestionInput,
+}
+
+export const Suggestion = () => <SuggestionInput />
