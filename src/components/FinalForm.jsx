6abbb553295fb15c9dbcde66086ea9d481621258
Maria <pashkovichma@gmail.com>
2023-11-20 12:03:21 +0100

    refactor css

--- src/components/FinalForm.jsx
+++ src/components/FinalForm.jsx
@@ -5,25 +5,20 @@ function FinalForm({
   value,
   reset
 }) {
-  const state = Object.entries(value.data);
-
   return (
     <>
       <h1 className={styles.title}>
         Текущий стейт:
       </h1>
       <ReactJson src={value} />
-      <div className={styles.container}>
-        <div>
-          <button
-          onClick={reset}
-          className={styles.button}
-          >
-            Начать сначала
-          </button>
-        </div>
+      <div>
+        <button
+        onClick={reset}
+        className={styles.button}
+        >
+          Начать сначала
+        </button>
       </div>
-      
     </>
   )
 }
